44b6a08edd85566f5acf4fc48ae132f73027aa5a
nayeonkiim <wkdrnsldk98@naver.com>
2021-05-29 15:27:09 +0900

    스레드 적용

--- pyFaceEncryption/camAndEncrypt/main_cam.py
+++ pyFaceEncryption/camAndEncrypt/main_cam.py
@@ -3,138 +3,160 @@ from datetime import datetime
 import os
 import cv2
 import base64
+import threading
 from camAndEncrypt.main_img import encryptImg, macInsert
 
-xml = 'C:/Users/hyeri/Desktop/Capstone/Security/Real-time-face-recognition-and-mosaic-using-deep-learning-master/haarcascades/haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier(xml)
 
-cap = cv2.VideoCapture(0)  # 노트북 웹캠을 카메라로 사용
-cap.set(3, 640)  # 너비
-cap.set(4, 480)  # 높이
+def toencrypt(cap):
+    cond = True
+    while (cond):
+        # 얼굴 인식
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)  # 좌우 대칭
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-time = datetime.now()
-i = 0
-while (True):
-    # 얼굴 인식
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)  # 좌우 대칭
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.05, 5)
-    print("Number of faces detected: " + str(len(faces)))
-    faceArr = []
-    for (x, y, w, h) in faces:
-        faceArr.append((x, y, w, h))
-
-    #얼굴 인식된 경우
-    if len(faces):
-        if ret:
-            print("time : " + str(time))
-            time2 = datetime.now()
-            print("time2 : " + str(time2))  #0.587272 초
-            # 시간값도 설정하기
-            if (time2-time).seconds >= 60 or i == 0:
-                # 해당 경로에 폴더가 존재하지 않으면 새로 생성
-                dt = datetime.now()
-
-                # 월
-                month = str(dt.month)
-                # month를 05, 08 형태로 만들기
-                if (month.__len__() == 1):
-                    month = '0' + month
-
-                # 일
-                day = str(dt.day)
-                # day를 05, 08 형태로 만들기
-                if (day.__len__() == 1):
-                    day = '0' + day
-
-                folderName = month + "m" + day
-                print(folderName)
-                path = "C:/opencv/" + folderName
-
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-                    print('파일 생성 완료')
-
-                # 시
-                hour = str(dt.hour)
-                # hour를 05, 08 형태로 만들기
-                if (hour.__len__() == 1):
-                    hour = '0' + hour
-
-                # 분
-                minute = str(dt.minute)
-                # minute를 05, 08 형태로 만들기
-                if (minute.__len__() == 1):
-                    minute = '0' + minute
-
-                # 사진 캡처된 시간
-                fileName = hour + "." + minute
-                print(time)
-
-                name = "C:/opencv/" + folderName + "/" + fileName
-
-                # return : retval(압축 결과 : True / False), img(인코딩된 이미지)
-                result, img = cv2.imencode('.bmp', frame, [cv2.IMWRITE_WEBP_QUALITY, 100])
-                # 이미지를 base64로 바꾸기
-                base64_string = base64.b64encode(img)
-
-                # 사진 암호화(base64 URL)
-                x,y = encryptImg(base64_string, name)
-
-                # mac -> String
-                macStr = ''
-
-                for idx, c in enumerate(y):
-                    macStr += "{:02x}".format(int(c))
-
-                print('macStr : ', macStr)
-
-                # 이미지 mac(y)을 DB에 넣기
-                macInsert(str(dt.year), month, day, hour, minute, macStr)
-
-                face = cv2.imread(name)
-                i += 1
-                time = time2
-
-            else:
-                continue;
-
+        faces = face_cascade.detectMultiScale(frame, 1.05, 5)
+        print("Number of faces detected: " + str(len(faces)))
+        faceArr = []
         for (x, y, w, h) in faces:
-            print(x, y, w, h)
-            face_img = frame[y:y + h, x:x + w]  # 인식된 얼굴 이미지 crop
-            face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.04, fy=0.04)  # 축소
-            face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA)  # 확대
-            frame[y:y + h, x:x + w] = face_img  # 인식된 얼굴 영역 모자이크 처리
+            faceArr.append((x, y, w, h))
 
-
-        file_path = 'C:/Users/hyeri/Desktop/record.avi'  # 저장할 파일 경로 이름 ---①
-        fps = 30.0  # FPS, 초당 프레임 수
-        fourcc = cv2.VideoWriter_fourcc(*'DIVX')  # 인코딩 포맷 문자
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        size = (int(width), int(height))  # 프레임 크기
-        out = cv2.VideoWriter(file_path, fourcc, fps, size)  # VideoWriter 객체 생성
-
-        while True:
+        i = 0
+        time = datetime.now()
+        #얼굴 인식된 경우
+        if len(faces):
             if ret:
-                cv2.imshow('camera-recording', frame)
-                out.write(frame)  # 파일 저장
-                if cv2.waitKey(int(1000 / fps)) != -1:
-                    break
-            else:
-                print("no frame!")
+                print("time : " + str(time))
+                time2 = datetime.now()
+                print("time2 : " + str(time2))  #0.587272 초
+                # 시간값도 설정하기
+                if (time2-time).seconds >= 60 or i == 0:
+                    # 해당 경로에 폴더가 존재하지 않으면 새로 생성
+                    dt = datetime.now()
+
+                    # 월
+                    month = str(dt.month)
+                    # month를 05, 08 형태로 만들기
+                    if (month.__len__() == 1):
+                        month = '0' + month
+
+                    # 일
+                    day = str(dt.day)
+                    # day를 05, 08 형태로 만들기
+                    if (day.__len__() == 1):
+                        day = '0' + day
+
+                    folderName = month + "m" + day
+                    print(folderName)
+                    path = "C:/opencv/" + folderName
+
+                    if not os.path.isdir(path):
+                        os.mkdir(path)
+                        print('파일 생성 완료')
+
+                    # 시
+                    hour = str(dt.hour)
+                    # hour를 05, 08 형태로 만들기
+                    if (hour.__len__() == 1):
+                        hour = '0' + hour
+
+                    # 분
+                    minute = str(dt.minute)
+                    # minute를 05, 08 형태로 만들기
+                    if (minute.__len__() == 1):
+                        minute = '0' + minute
+
+                    # 사진 캡처된 시간
+                    fileName = hour + "." + minute
+                    print(time)
+
+                    name = "C:/opencv/" + folderName + "/" + fileName
+
+                    # return : retval(압축 결과 : True / False), img(인코딩된 이미지)
+                    result, img = cv2.imencode('.bmp', frame, [cv2.IMWRITE_WEBP_QUALITY, 100])
+                    # 이미지를 base64로 바꾸기
+                    base64_string = base64.b64encode(img)
+
+                    # 사진 암호화(base64 URL)
+                    x,y = encryptImg(base64_string, name)
+
+                    # mac -> String
+                    macStr = ''
+
+                    for idx, c in enumerate(y):
+                        macStr += "{:02x}".format(int(c))
+
+                    print('macStr : ', macStr)
+
+                    # 이미지 mac(y)을 DB에 넣기
+                    macInsert(str(dt.year), month, day, hour, minute, macStr)
+
+                    face = cv2.imread(name)
+                    i += 1
+                    time = time2
+
+def WebcamVieoWriter(cap):
+        # 얼굴 인식
+        width = int(cap.get(3))
+        height = int(cap.get(4))
+
+        # 코덱정보를 나타냄 아래의 두줄과 같이 사용할 수 있음.
+        # 둘중 어느것을 쓰든 상관없음.
+        # 여러가지의 코덱종류가 있지만 윈도우라면 DIVX 를 사용
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        # fourcc = cv2.VideoWriter_fourcc('D','I','V','X')
+
+        # 비디오 저장을 위한 객체를 생성해줌.
+        out = cv2.VideoWriter('SaveVideo.mp4', fourcc, 20.0, (width, height))
+
+        while (True):
+            ret, frame = cap.read()
+
+
+            if not ret:
+                print("비디오 읽기 오류")
                 break
 
+            frame = cv2.flip(frame, 1)  # 좌우 대칭
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(frame, 1.05, 5)
+            print("Number of faces detected: " + str(len(faces)))
+            for (x, y, w, h) in faces:
+                print(x, y, w, h)
+                face_img = frame[y:y + h, x:x + w]  # 인식된 얼굴 이미지 crop
+                face_img = cv2.resize(face_img, dsize=(0, 0), fx=0.04, fy=0.04)  # 축소
+                face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA)  # 확대
+                frame[y:y + h, x:x + w] = face_img  # 인식된 얼굴 영역 모자이크 처리
+
+            # 비디오 프레임이 정확하게 촬영되었으면 화면에 출력하여줌
+            cv2.imshow('video', frame)
+            # 비디오 프레임이 제대로 출력되면 해당파일에 프레임을 저장
+            out.write(frame)
+
+            # ESC키값을 입력받으면 녹화종료 메세지와 함께 녹화종료
+            k = cv2.waitKey(1)
+            if (k == 27):
+                print('녹화 종료')
+                break
+
+        # 비디와 관련 장치들을 다 닫아줌.
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
 
-        # k = cv2.waitKey(30) & 0xff
-        # if k == 27:  # Esc 키를 누르면 종료
-        #     break
 
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    xml = '/haarcascades/haarcascade_frontalface_default.xml'
+    face_cascade = cv2.CascadeClassifier(xml)
 
+    cap = cv2.VideoCapture(0)  # 노트북 웹캠을 카메라로 사용
+    cap.set(3, 640)  # 너비
+    cap.set(4, 480)  # 높이
 
+    mythread1 = threading.Thread(target=toencrypt, args=(cap,))
+    mythread2 = threading.Thread(target=WebcamVieoWriter, args=(cap,))
 
+    mythread1.start()
+    mythread2.start()
